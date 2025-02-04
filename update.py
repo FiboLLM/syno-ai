555a22afcb47c7f7628d7234e1be70564d25e596
0xPolskaCebula <191962010+0xPolskaCebula@users.noreply.github.com>
2025-02-04 01:23:57 -0700

    Update update.py

--- update.py
+++ update.py
@@ -15,7 +15,7 @@ def main():
     script_dir = os.path.dirname(os.path.abspath(__file__))
     os.chdir(script_dir)
 
-    print("Updating Project Alice...")
+    print("Updating Project Fibo...")
 
     # Pull latest changes from the repository
     print("Pulling latest changes from the repository...")
@@ -26,8 +26,8 @@ def main():
     run_command("docker-compose build --no-cache", "Failed to rebuild Docker images.")
 
     # Run the run.py script
-    print("Starting Project Alice...")
-    run_command("python run.py", "Failed to start Project Alice.")
+    print("Starting Project Fibo...")
+    run_command("python run.py", "Failed to start Project Fibo.")
 
 if __name__ == "__main__":
     main()
